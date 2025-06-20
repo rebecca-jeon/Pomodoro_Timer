90b37c9d15a89d78f52794bc327425d9b6adc9bb
Rebecca Jeon <she4115@gmail.com>
2025-06-20 00:09:06 -0700

    Add: Date + Time Styling

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const { create } = require('domain');
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Pomodoro Timer',
-        width: 800,
-        height: 600
+        width: 1280,
+        height: 832
     });
     mainWindow.loadFile(path.join(__dirname, './renderer/tasks.html'));
 }
